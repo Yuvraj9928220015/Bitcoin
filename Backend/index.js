35b538ddc059ecffea780b762deec8fb41b5cc4b
Yuvraj9928220015 <yuvrajsharma190105@gmail.com>
2025-10-13 10:10:24 +0530

    localhost change

--- Backend/index.js
+++ Backend/index.js
@@ -26,6 +26,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://localhost:5175",
         "http://bitcoinbutik.com",
         "https://bitcoinbutik.com",
         "https://www.bitcoinbutik.com"
